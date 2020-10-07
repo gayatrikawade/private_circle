f3c0d5ff60913eddcdd5cb57b14e927eaa551f89
Gayatri Kawade <gayatrikawade80@gmail.com>
2020-10-08 02:19:48 +0530

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'private-circle-assighnment';
+  details = [];
+
+  items = [
+    {
+      'date': 'April 15',
+      'name': 'Aeione',
+      'entitie': 5,
+      'details': ["jabvkkkkkkkk hjdsfbasf jbfjaef",
+        "vbjdvbnbfhj hjbdfjhaf n fjdf"]
+    },
+    {
+      'date': 'April 15',
+      'name': 'Aeione',
+      'entitie': 5,
+      'details': ["jabvkkkkkkkk hjdsfbasf jbfjaef",
+        "vbjdvbnbfhj hjbdfjhaf n fjdf", "vbjdvbnbfhj hjbdfjhaf n fjdf", "vbjdvbnbfhj hjbdfjhaf n fjdf"]
+    },
+    {
+      'date': 'April 15',
+      'name': 'Aeione',
+      'entitie': 5,
+      'details': ["jabvkkkkkkkk hjdsfbasf jbfjaef",
+        "vbjdvbnbfhj hjbdfjhaf n fjdf"]
+    },
+    {
+      'date': 'April 15',
+      'name': 'Aeione',
+      'entitie': 5,
+      'details': ["jabvkkkkkkkk hjdsfbasf jbfjaef",
+        "vbjdvbnbfhj hjbdfjhaf n fjdf"]
+    },
+    {
+      'date': 'April 15',
+      'name': 'Aeione',
+      'entitie': 5,
+      'details': ["jabvkkkkkkkk hjdsfbasf jbfjaef",
+        "vbjdvbnbfhj hjbdfjhaf n fjdf"]
+    },
+    {
+      'date': 'April 15',
+      'name': 'Aeione',
+      'entitie': 5,
+      'details': ["jabvkkkkkkkk hjdsfbasf jbfjaef",
+        "vbjdvbnbfhj hjbdfjhaf n fjdf"]
+    },
+    {
+      'date': 'April 15',
+      'name': 'Aeione',
+      'entitie': 5,
+      'details': ["jabvkkkkkkkk hjdsfbasf jbfjaef",
+        "vbjdvbnbfhj hjbdfjhaf n fjdf"]
+    },
+
+  ];
+
+  showDetails(details) {
+    console.log("bdvjdd");
+
+    this.details = details;
+  }
 }
